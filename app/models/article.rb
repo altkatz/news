a7f1c500aec1cad879784db582ab281c6f20e2de
C.Li <chrislitex@gmail.com>
2013-09-07 16:18:45 +0800

    article validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ActiveRecord::Base
   has_and_belongs_to_many :columns, :join_table => :columns_articles
+  validates :title, :summary, presence: true
+  validates :status, inclusion: { in: %w(draft, published, banned),
+    message: "%{value} is not a valid status" }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,6 @@ require 'spec_helper'
 
 describe Article do
   it { should have_and_belong_to_many(:columns) }
-  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:title) }
+  it { should ensure_inclusion_of(:status).in_array(%w(draft, published, banned)) }
 end
